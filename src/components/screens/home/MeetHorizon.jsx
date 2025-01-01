55e0d2799e73070369d840797e49adc47dfe92f9
afsanaeva <afsanaeva05@gmail.com>
2025-01-02 00:20:44 +0600

    super engagment slide half done

--- src/components/screens/home/MeetHorizon.jsx
+++ src/components/screens/home/MeetHorizon.jsx
@@ -1,33 +1,38 @@
 "use client";
+
 import React, { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 import { motion } from "framer-motion";
 import InfoCard2 from "@/components/custom-ui/InfoCard2";
 import { fadeTop } from "@/components/layout/Header";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import Image from "next/image";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel";
-import Image from "next/image";
 
 const slides = [
   {
     title: "Horizon Shorts",
     img: "/assets/home/hero/slide-1.png", // Replace with your image path
-    subText: `Drive instant engagement with the world’s most consumed content format.`,
+    text: "Grow with short - form videos",
+    subText:
+      "Drive instant engagement with the world’s most consumed content format.",
   },
   {
     title: "Horizon Stories",
     img: "/assets/home/hero/slide-2.png", // Replace with your image path
-    subText: `Create immersive stories that captivate users and deepen connections.`,
+    text: "Highlight what matters to your customers",
+    subText:
+      "Create immersive stories that captivate users and deepen connections.",
   },
   {
     title: "Horizon Feeds",
-    img: "/assets/home/hero/slide-3.png ", // Replace with your image path
-    subText: `Build infinite scrolling feeds to keep users engaged and returning.`,
+    img: "/assets/home/hero/slide-3.png", // Replace with your image path
+    text: "Build an engaged community",
+    subText:
+      "Build infinite scrolling feeds to keep users engaged and returning.",
   },
 ];
 
@@ -56,17 +61,18 @@ const HeroSlides = ({ slides }) => {
     >
       <Carousel
         setApi={setApi}
-        className="pointer-events-auto relative"
+        className="relative"
         opts={{
           loop: false,
           dragFree: true,
         }}
       >
-        <CarouselContent className="items-center rounded-[36px] bg-[#F7FBFE] py-3">
+        <CarouselContent className="items-center rounded-[36px] bg-[#F7FBFE]">
           {slides.map((slide, index) => (
             <Slide3
               key={index}
               title={slide.title}
+              text={slide.text}
               image={slide.img}
               index={index}
               subText={slide.subText}
@@ -76,58 +82,83 @@ const HeroSlides = ({ slides }) => {
         </CarouselContent>
       </Carousel>
 
-      {/* Navigation */}
-      <div className="absolute bottom-4 right-4 mt-10 flex gap-2">
+      {/* Navigation Buttons */}
+      <div className="absolute right-4 mr-10 mt-3 flex gap-2">
         <button
           onClick={() => api?.scrollPrev()}
           className={cn(
             "flex size-10 items-center justify-center rounded-full bg-white text-gray-700 shadow-md transition hover:bg-gray-200",
-            { "opacity-50 cursor-not-allowed": isStart }
+            { "opacity-50 cursor-not-allowed": isStart } // Disable button styling
           )}
           disabled={isStart}
+          aria-label="Previous Slide"
         >
-          &#8592;
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="size-6"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <polyline points="15 18 9 12 15 6" />
+          </svg>
         </button>
         <button
           onClick={() => api?.scrollNext()}
           className={cn(
             "flex size-10 items-center justify-center rounded-full bg-white text-gray-700 shadow-md transition hover:bg-gray-200",
-            { "opacity-50 cursor-not-allowed": isEnd }
+            { "opacity-50 cursor-not-allowed": isEnd } // Disable button styling
           )}
           disabled={isEnd}
+          aria-label="Next Slide"
         >
-          &#8594;
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="size-6"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <polyline points="9 18 15 12 9 6" />
+          </svg>
         </button>
       </div>
     </motion.div>
   );
 };
 
-const Slide3 = ({ title, image, subText, index, isSelected }) => {
+const Slide3 = ({ title, image, text, subText, index, isSelected }) => {
   return (
     <CarouselItem
       key={index}
-      className={cn("flex flex-col items-center", {
-        "opacity-100 scale-100": isSelected,
-        "opacity-50 scale-90": !isSelected,
-      })}
-      style={{
-        transition: "transform 0.3s ease-in-out",
-      }}
+      className={cn(
+        "flex flex-col md:flex-row items-center md:justify-between p-6 pl-20 bg-[#F7FBFE] rounded-[36px] transition-transform container",
+        {
+          "opacity-100 scale-100": isSelected,
+          "opacity-50 scale-90": !isSelected,
+        }
+      )}
     >
-      <div className=" flex justify-between ">
-        <div>
-          <h3 className="font-40px mt-4 font-semibold">{title}</h3>
-          <p className="font-28px mt-6 text-center">{subText}</p>
-        </div>
-        <Image
-          src={image}
-          alt={title}
-          width={258}
-          height={532}
-          className="rounded-40px"
-        />
+      <div className="space-y-4 text-center md:max-w-[50%] md:text-left ">
+        <h3 className="font-52px font-semibold text-[#15234E]">{title}</h3>
+        <h3 className="font-32px font-semibold text-[#15234E]">{text}</h3>
+        <p className="font-22px border-b border-gray-300 pb-2 text-[#15234E]">
+          {subText}
+        </p>
       </div>
+      <Image
+        src={image}
+        alt={title}
+        width={558}
+        height={532}
+        className="mt-4 rounded-[40px] md:mt-0"
+      />
     </CarouselItem>
   );
 };
@@ -160,7 +191,6 @@ const MeetHorizon = () => {
           headingClassName="font-28px font-normal text-left"
         />
       </div>
-
       <HeroSlides slides={slides} />
     </div>
   );
